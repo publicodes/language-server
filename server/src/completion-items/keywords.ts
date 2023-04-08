f0058ae0df5f0494fbbe534f667363cf9837fdb5
Emile Rolley <emile.rolley@tuta.io>
2023-04-08 18:33:22 +0200

    feat(server/completion): add more keywords

--- server/src/completion-items/keywords.ts
+++ server/src/completion-items/keywords.ts
@@ -1,5 +1,6 @@
 import { CompletionItem } from "vscode-languageserver/node";
 
+// Keywords extracted from: https://github.com/betagouv/publicodes/blob/82a31f7e70837a02628cb1b758b87128819e8bff/packages/core/source/rule.ts#L18-L42
 export const keywords: CompletionItem[] = [
   {
     label: "titre",
@@ -31,6 +32,75 @@ références:
 ]  références:
     - https://avenirclimatique.org/action/
     - https://www.statistiques.developpement-durable.gouv.fr/estimation-de-lempreinte-carbone-de-1995-2019?rubrique=27&dossier=1286
+\`\`\`
+
+`,
+  },
+  {
+    label: "valeur",
+    documentation: `
+Permet de définir la valeur de la règle.
+
+### Exemple
+
+\`\`\`yaml
+valeur: 200€
+\`\`\`
+
+`,
+  },
+  {
+    label: "question",
+    documentation: `
+Permet de définir la question posée à l'utilisateur.
+
+### Exemple
+
+\`\`\`yaml
+question: Quel est votre âge ?
+\`\`\`
+
+`,
+  },
+  {
+    label: "résumé",
+    documentation: `
+Permet de définir le résumé de la règle.
+
+### Exemple
+
+\`\`\`yaml
+résumé: Le montant de la prime de vacances est de 200€
+\`\`\`
+
+`,
+  },
+  {
+    label: "icônes",
+    documentation: `
+Permet de définir les icônes de la règle.
+
+### Exemple
+
+\`\`\`yaml
+icônes: 🏖️
+\`\`\`
+
+`,
+  },
+  {
+    label: "suggestions",
+    documentation: `
+Permet de définir les suggestions de la règle.
+
+### Exemple
+
+\`\`\`yaml
+suggestions:
+  Paris🔁Athènes: 6000
+  Brest🔁Nice: 3000
+  Paris🔁Marseille: 1600
+  Bordeaux🔁Lyon: 1100 
 \`\`\`
 
 `,
