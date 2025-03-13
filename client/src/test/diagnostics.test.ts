80972b9add260600c18075c8d8aa8d6c1e3ee413
Johan Girod <johan@girod.email>
2025-03-13 17:48:00 +0100

    chore: fix lint

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -82,7 +82,7 @@ function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
 
 async function testDiagnostics(
   docUri: vscode.Uri,
-  expectedDiagnostics: vscode.Diagnostic[]
+  expectedDiagnostics: vscode.Diagnostic[],
 ) {
   await activate(docUri);
 
